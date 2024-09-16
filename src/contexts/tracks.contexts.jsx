09481e193b710041f4a7de07b347f58cb354302d
Ramsha <fnuramsha@gmail.com>
2024-09-16 13:40:33 +0200

    resolved React Hooks issues

--- src/contexts/tracks.contexts.jsx
+++ src/contexts/tracks.contexts.jsx
@@ -2,8 +2,8 @@ import { createContext, useState } from "react";
 
 export const tracksContext = createContext();
 
-export const tracksProvider = (children) => {
-  [val, setVal] = useState();
+export const TracksProvider = ({ children }) => {
+  const [val, setVal] = useState("");
   const value = { val, setVal };
   return (
     <tracksContext.Provider value={value}> {children} </tracksContext.Provider>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { TracksProvider } from "./contexts/tracks.contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TracksProvider>
+      <App />
+    </TracksProvider>
   </React.StrictMode>
 );
 
