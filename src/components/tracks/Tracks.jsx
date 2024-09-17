286fb179d0c19c9c85ac9da63f8a03f0d13a2bce
Ramsha <fnuramsha@gmail.com>
2024-09-17 14:51:35 +0200

    added function that fetch top 10 songs

--- src/components/tracks/Tracks.jsx
+++ src/components/tracks/Tracks.jsx
@@ -2,13 +2,17 @@ import React, { useContext } from "react";
 import { tracksContext } from "../../contexts/tracks.contexts";
 
 const Tracks = () => {
-  const { tracks } = useContext(tracksContext);
+  const { getData, trackList } = useContext(tracksContext);
+  const clickFunc = (e) => {
+    getData(trackList);
+  };
   return (
     <div className="text-center">
       <h3>Top 10 Tracks</h3>
-      console.log(tracks)
+
       <div class="container">
         <div class="row align-items-center"></div>
+        <button onClick={clickFunc}>Click me</button>
       </div>
     </div>
   );
--- src/contexts/tracks.contexts.jsx
+++ src/contexts/tracks.contexts.jsx
@@ -1,20 +1,27 @@
 import { createContext, useState } from "react";
 import axios from "axios";
 
-const getData = () => {
-  axios
-    .get()
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err));
+const getData = async (trackList) => {
+  try {
+    const response = await axios.get(
+      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&
+              apikey=${process.env.REACT_APP_MM_KEY}`
+    );
+    trackList = response.data;
+    console.log(trackList);
+    return trackList;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 export const tracksContext = createContext({
-  tracks: 10,
+  tracksList: 0,
 });
 
 export const TracksProvider = ({ children }) => {
-  const [trackList, setTrackList] = useState(tracks);
-  const value = { trackList, setTrackList };
+  const [trackList, setTrackList] = useState("");
+  const value = { trackList, setTrackList, getData };
   return (
     <tracksContext.Provider value={value}> {children} </tracksContext.Provider>
   );
